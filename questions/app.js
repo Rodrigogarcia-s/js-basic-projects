8c9a69481ddafb6f56dd0f77315461e391e91f89
Codewithem <132796832+code-withem@users.noreply.github.com>
2023-07-07 17:34:44 -0300

    Update app.js

--- questions/app.js
+++ questions/app.js
@@ -1,9 +1,34 @@
-const btns = document.querySelectorAll(".question-btn");
+//Opcion-1
+// const btns = document.querySelectorAll(".question-btn");
 
-btns.forEach(function (btn) {
-    btn.addEventListener('click', function (e) {
-        const question =e.currentTarget.parentElement.parentElement;
+// btns.forEach(function (btn) {
+//     btn.addEventListener('click', function (e) {
+//         const question =e.currentTarget.parentElement.parentElement;
     
-        question.classList.toggle("show-text")
+//         question.classList.toggle("show-text")
+//     });
+// });
+
+//Opcion-2
+
+const questions=document.querySelectorAll('.section-centre');
+
+questions.forEach(function(question){
+    const btn = question.querySelector('.question-btn')
+    btn.addEventListener('click', function(){
+
+      questions.forEach(function(item){
+        if (item !== question){
+            item.classList.remove('show-text');
+        }
+
+      });  
+
+
+
+
+      question.classList.toggle("show-text");
+    
     });
 });
+
